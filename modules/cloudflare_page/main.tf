3729728dc57fff665995daa48ded4dacbcf8dd7d
Jonathan Tessman <jtessman@sourceallies.com>
2025-09-24 17:24:02 -0500

    more vars

--- modules/cloudflare_page/main.tf
+++ modules/cloudflare_page/main.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_pages_project" "static_site" {
   account_id        = data.cloudflare_accounts.main.accounts[0].id
   name              = var.project_name
-  production_branch = "main"
+  production_branch = var.github_branch
 
   # Source Configuration 
   # (uses main branch to trigger deployments via GitHub integration)
@@ -10,7 +10,7 @@ resource "cloudflare_pages_project" "static_site" {
     config {
       owner                         = var.github_owner
       repo_name                    = var.github_repo
-      production_branch            = "main"
+      production_branch            = var.github_branch
       pr_comments_enabled          = true
       deployments_enabled          = true
       production_deployment_enabled = true
@@ -20,11 +20,8 @@ resource "cloudflare_pages_project" "static_site" {
 
   # Build configuration for static HTML
   build_config {
-    build_command       = ""
-    destination_dir     = "app"
-    root_dir           = ""
-    web_analytics_tag  = ""
-    web_analytics_token = ""
+    build_command       = var.build_command
+    destination_dir     = var.application_directory
   }
 
   # Deployment configuration
